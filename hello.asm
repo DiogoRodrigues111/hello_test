f1dc29d83a5317683cb43a36b32b8bbf6812692d
Diogo R Roessler <drroessler@hotmail.com.br>
2022-07-31 23:27:43 -0300

    hello, world

---
+++ hello.asm
@@ -0,0 +1,19 @@
+global start
+
+section .text
+
+start:
+    MOV ECX, __msg
+    MOV EDX, __msg_len
+    MOV EAX, 4 ; output (sys_write)
+    MOV EBX, 1
+    int 80h
+
+    MOV EAX, 1  ; select always for next iterations.
+    int 80h
+
+section .data
+
+; create an variables.
+__msg: DB "Hello, world!", 0xF
+__msg_len EQU $ - __msg
